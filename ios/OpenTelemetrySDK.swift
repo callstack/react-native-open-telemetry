6971af021ce05fb5c954e3d226881e96f323c3eb
Adam Horodyski <ad.horodyski@gmail.com>
2025-04-06 22:24:20 +0200

    feat: sdk resource param

--- ios/OpenTelemetrySDK.swift
+++ ios/OpenTelemetrySDK.swift
@@ -15,7 +15,7 @@ public final class OpenTelemetrySDK: NSObject {
         public var version: String = "0.0.0"
         public var environment: String = "development"
     }
-    
+
     public static func get() -> OpenTelemetry {
         return OpenTelemetry.instance
     }
@@ -24,15 +24,32 @@ public final class OpenTelemetrySDK: NSObject {
         let options = Options()
         configure(options)
 
+        sdkResource.merge(
+            other: Resource(attributes: [
+                ResourceAttributes.serviceName.rawValue: AttributeValue.string(
+                    options.name
+                ),
+                ResourceAttributes.serviceVersion.rawValue:
+                    AttributeValue.string(
+                        options.version
+                    ),
+                // TODO: try unifying to `deploymentEnvironment` across every platform
+                ResourceAttributes.RawValue("environment"):
+                    AttributeValue.string(
+                        options.environment
+                    ),
+            ])
+        )
+
         let logSpanExporter = options.debug ? StdoutSpanExporter() : nil
-        let spanExporter = MultiSpanExporter(spanExporters: [logSpanExporter].compactMap { $0 })
+        let spanExporter = MultiSpanExporter(
+            spanExporters: [logSpanExporter].compactMap { $0 }
+        )
         spanProcessor = BatchSpanProcessor(spanExporter: spanExporter)
-        let tracerProviderBuilder = TracerProviderBuilder()
-        _ = tracerProviderBuilder.with(resource: sdkResource)
-        if let spanProcessor = spanProcessor {
-            _ = tracerProviderBuilder.add(spanProcessor: spanProcessor)
-        }
-        let tracerProvider = tracerProviderBuilder.build()
+        let tracerProvider = TracerProviderBuilder()
+            .with(resource: sdkResource)
+            .add(spanProcessor: spanProcessor!)
+            .build()
 
         OpenTelemetry.registerTracerProvider(tracerProvider: tracerProvider)
     }
