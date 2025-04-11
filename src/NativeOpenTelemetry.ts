709e56d8fcf1ead524fd6c8eddf6c2b4377289c2
Adam Horodyski <ad.horodyski@gmail.com>
2025-04-11 22:38:14 +0200

    fix: codegen so web and native will live happily ever after

--- src/NativeOpenTelemetry.ts
+++ src/NativeOpenTelemetry.ts
@@ -1,6 +1,24 @@
-import type { Spec } from "./NativeOpenTelemetry.native";
+import type { Attributes, HrTime, SpanContext, SpanKind, SpanStatus } from "@opentelemetry/api";
+import type { InstrumentationScope } from '@opentelemetry/core';
+import type { ScopeMetrics } from "@opentelemetry/sdk-metrics";
+import { TurboModuleRegistry, type TurboModule } from "react-native";
 
-export default {
-  exportTraces: () => {},
-  exportMetrics: () => {},
-} as Spec;
+export interface Spec extends TurboModule {
+  exportTraces(
+    spans: {
+      name: string;
+      spanContext: SpanContext;
+      status: SpanStatus;
+      startTime: HrTime;
+      endTime: HrTime;
+      kind: SpanKind;
+      attributes: Attributes;
+      parentSpanId?: string;
+      ended: boolean;
+      instrumentationScope: InstrumentationScope;
+    }[],
+  ): void;
+  exportMetrics(metrics: ScopeMetrics[]): void;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>("OpenTelemetry");
