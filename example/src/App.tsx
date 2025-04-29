04e738ae7a56294646800d53e3bc8c3d16b6924a
Adam Horodyski <ad.horodyski@gmail.com>
2025-04-29 11:14:32 +0200

    chore: refactor the example app to nest contexts

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -32,28 +32,29 @@ export default function App() {
         style={{ padding: 16, backgroundColor: "lightgray", borderRadius: 8 }}
         onPress={async () => {
           console.log("Starting a long, parent span...");
-          const parentSpan = tracer.startSpan("my-js-homepage-span");
-          const ctx = sdk.trace.setSpan(sdk.context.active(), parentSpan);
-          parentSpan.setAttributes({
-            platform: "js",
-            userId: 123,
-            userType: "admin",
+
+          tracer.startActiveSpan("my-js-homepage-span", async (parentSpan) => {
+            parentSpan.setAttributes({
+              platform: "js",
+              userId: 123,
+              userType: "admin",
+            });
+
+            const childSpan = tracer.startSpan(
+              "my-js-homepage-child-span",
+              { attributes: { type: "child" } },
+            );
+            childSpan.end();
+
+            // sleep for a random 1-3 seconds
+            const sleepTime = Math.floor(Math.random() * 3000) + 1000;
+            console.log(`Sleeping for ${sleepTime}ms`);
+            await new Promise((resolve) => setTimeout(resolve, sleepTime));
+
+            console.log("Span ended");
+            parentSpan.end();
           });
 
-          const childSpan = tracer.startSpan(
-            "my-js-homepage-child-span",
-            { attributes: { type: "child" } },
-            ctx
-          );
-          childSpan.end();
-
-          // sleep for a random 1-3 seconds
-          const sleepTime = Math.floor(Math.random() * 3000) + 1000;
-          console.log(`Sleeping for ${sleepTime}ms`);
-          await new Promise((resolve) => setTimeout(resolve, sleepTime));
-
-          parentSpan.end();
-          console.log("Span ended");
         }}
       >
         <Text>Start a span</Text>
